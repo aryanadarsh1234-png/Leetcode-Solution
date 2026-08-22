42858e7eb69639784eda334921ed0a2fe4532510
Aryan Adarsh <aryanadarsh1234@gmail.com>
2026-08-22 22:44:42 +0530

    Time: 0 ms (100%), Space: 43 MB (67.16%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -11,14 +11,14 @@
 class Solution {
     public ListNode swapPairs(ListNode head) {
 
+        ListNode dummy = new ListNode(-1);
 
-        ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode prev = dummy;
 
         while(prev.next!=null && prev.next.next!=null){
             ListNode first = prev.next;
-            ListNode second = first.next;
+            ListNode second = prev.next.next;
 
             first.next = second.next;
             second.next = first;
